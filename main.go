b93c562bbcd061594156f144cbdc0cd8d3fe356a
Eliseo-Baroni-97 <elibaroni97@gmail.com>
2025-08-17 17:51:27 -0300

    corrigiendojwt

--- main.go
+++ main.go
@@ -183,8 +183,8 @@ func main() {
 
 		fmt.Println("🔧 JWT_SECRET en runtime:", os.Getenv("JWT_SECRET"))
 
-		secret := os.Getenv("MY_SUPER_JWT")
-		fmt.Println("🔎 Valor de MY_SUPER_JWT:", secret)
+		secret := os.Getenv("JWT_SECRET")
+
 		if secret == "" {
 			fmt.Println("❌ JWT_SECRET no definido")
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "JWT no configurado"})
